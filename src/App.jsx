4f7205121a77967eb23d0f9c3adb66c16568b2e4
chanhman <hello@chanh.design>
2025-01-30 20:19:07 -0700

    lower res, no audio

--- src/App.jsx
+++ src/App.jsx
@@ -8,16 +8,16 @@ import 'swiper/css';
 import VideoJS from './VideoJS';
 
 const videoSources = [
-  { src: '/INFINITY SCROLL_1.mp4' },
-  { src: '/INFINITY SCROLL_2.mp4' },
-  { src: '/INFINITY SCROLL_3.mp4' },
-  { src: '/INFINITY SCROLL_4.mp4' },
-  { src: '/INFINITY SCROLL_5.mp4' },
-  { src: '/INFINITY SCROLL_6.mp4' },
-  { src: '/INFINITY SCROLL_7.mp4' },
-  { src: '/INFINITY SCROLL_8.mp4' },
-  { src: '/INFINITY SCROLL_9.mp4' },
-  { src: '/INFINITY SCROLL_10.mp4' },
+  { src: '/INFINITY SCROLL_1.webm' },
+  { src: '/INFINITY SCROLL_2.webm' },
+  { src: '/INFINITY SCROLL_3.webm' },
+  { src: '/INFINITY SCROLL_4.webm' },
+  { src: '/INFINITY SCROLL_5.webm' },
+  { src: '/INFINITY SCROLL_6.webm' },
+  { src: '/INFINITY SCROLL_7.webm' },
+  { src: '/INFINITY SCROLL_8.webm' },
+  { src: '/INFINITY SCROLL_9.webm' },
+  { src: '/INFINITY SCROLL_10.webm' },
 ];
 
 export default function App() {
